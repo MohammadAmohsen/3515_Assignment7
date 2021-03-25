1359d7e9e42384ada4307b95baa3e431275d310a
Mohammad Abdelmohsen <moeabd99@outlook.com>
2021-03-25 17:53:24 -0400

    Added Fragment

--- app/src/main/java/edu/temple/assignment7/MainActivity.java
+++ app/src/main/java/edu/temple/assignment7/MainActivity.java
@@ -16,20 +16,44 @@ public class MainActivity extends AppCompatActivity {
 
         BookList bookList = new BookList();
 
+
         Book book = new Book();
 
         book = new Book("Harry Potter","J.K Rowling");
+        bookList.add(book);
+
         book = new Book("Song Of Ice and Fire","George R. Martin");
+        bookList.add(book);
+
         book = new Book("The Lord of the Rings","J.R.R. Tolkien");
+        bookList.add(book);
+
         book = new Book("War and Peace","Leo Tolstoy");
+        bookList.add(book);
+
         book = new Book("Song of Solomon","Toni Morrison");
+        bookList.add(book);
+
         book = new Book("Ulysses"," James Joyce");
+        bookList.add(book);
+
         book = new Book("The Shadow of the Wind","Carlos Ruiz Zafon");
+        bookList.add(book);
+
         book = new Book("The Satanic Verses"," Salman Rushdie");
+        bookList.add(book);
+
         book = new Book("Don Quixote","Miguel de Cervantes");
+        bookList.add(book);
+
         book = new Book("The Golden Compass","Philip Pullman");
 
         bookList.add(book);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.mainActivityID,BookListFragment.newInstance(bookList))
+                .commit();
 
     }
 }
