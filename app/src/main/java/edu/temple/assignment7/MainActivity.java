687a14b735c327760e6f7475b2531f234052f5dd
Mohammad Abdelmohsen <moeabd99@outlook.com>
2021-03-25 16:11:30 -0400

    Added Book to BookList

--- app/src/main/java/edu/temple/assignment7/MainActivity.java
+++ app/src/main/java/edu/temple/assignment7/MainActivity.java
@@ -14,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BookList bookList = new BookList();
+
+        Book book = new Book();
+
+        book = new Book("Harry Potter","J.K Rowling");
+        book = new Book("Song Of Ice and Fire","George R. Martin");
+        book = new Book("The Lord of the Rings","J.R.R. Tolkien");
+        book = new Book("War and Peace","Leo Tolstoy");
+        book = new Book("Song of Solomon","Toni Morrison");
+        book = new Book("Ulysses"," James Joyce");
+        book = new Book("The Shadow of the Wind","Carlos Ruiz Zafon");
+        book = new Book("The Satanic Verses"," Salman Rushdie");
+        book = new Book("Don Quixote","Miguel de Cervantes");
+        book = new Book("The Golden Compass","Philip Pullman");
+
+        bookList.add(book);
+
     }
 }
