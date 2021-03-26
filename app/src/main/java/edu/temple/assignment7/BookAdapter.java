61705e008f060b2ef811f9d18e41d0d5badbf3d9
Mohammad Abdelmohsen <moeabd99@outlook.com>
2021-03-25 22:29:28 -0400

    End is near

--- app/src/main/java/edu/temple/assignment7/BookAdapter.java
+++ app/src/main/java/edu/temple/assignment7/BookAdapter.java
@@ -1,9 +1,11 @@
 package edu.temple.assignment7;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -26,22 +28,34 @@ public class BookAdapter extends ArrayAdapter {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         TextView textView;
         TextView textViewAuthor;
+        LinearLayout ll;
         if(convertView == null ){
             textView = new TextView(context);
+            ll = new LinearLayout(context);
             textView.setTextSize(22);
             textView.setPadding(15,20,0,20);
 
+            textViewAuthor = new TextView(context);
+            textViewAuthor.setTextSize(12);
+            textView.setTextColor(Color.GRAY);
+            textViewAuthor.setPadding(15,20,0,20);
 
-            }
+            ll.setOrientation(LinearLayout.VERTICAL);
+
+            ll.addView(textView);
+            ll.addView(textViewAuthor);
+        }
         else{
-            textView = (TextView) convertView;
-            textViewAuthor = (TextView) convertView;
+            ll = (LinearLayout) convertView;
+            textView = (TextView) ll.getChildAt(1);
+            textViewAuthor = (TextView)ll.getChildAt(1);
+
 
         }
        String title = ((Book)(getItem(position))).getTitle();
         String author = ((Book)(getItem(position))).getAuthor();
-        String BookDetails = title + "\r\n" + author;
-        textView.setText(BookDetails);
-        return textView;
+        textView.setText(title);
+        textViewAuthor.setText(author);
+        return ll;
     }
 }
